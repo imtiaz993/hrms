56258a61d66a361386049806b33a4e60e4472c30
Imtiaz Hussain <57477671+imtiaz993@users.noreply.github.com>
2025-12-05 14:36:37 +0500

    Updated build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6b3cf194a474561e"; }
+/******/ 		__webpack_require__.h = function() { return "949a7a78321cd6a8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
